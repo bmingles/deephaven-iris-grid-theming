67b9fb344affe7dfb4c45388e4104b99cb9ebd06
Brian Ingles <brianingles@deephaven.io>
2025-07-15 08:31:22 -0500

    preloadTheme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { ApiBootstrap } from '@deephaven/jsapi-bootstrap';
-import { ThemeProvider, type ThemeData } from '@deephaven/components';
+import {
+  preloadTheme,
+  ThemeProvider,
+  type ThemeData,
+} from '@deephaven/components';
 import { IrisGridThemeProvider } from '@deephaven/iris-grid';
 import './index.css';
 import { App } from './App.tsx';
+
+// Preload cached theme variables so that initial render remembers last used theme
+preloadTheme();
 
 const jsapiUrl = new URL('http://localhost:10000/jsapi/dh-core.js');
 const serverUrl = new URL(jsapiUrl.origin); // Use the same origin as the jsapiUrl
